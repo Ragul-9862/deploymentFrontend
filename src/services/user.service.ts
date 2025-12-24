0acc926e79d69c35a9361227f11f46651cdf90de
ragul-9862 <ragulragul9862@gmail.com>
2025-12-24 15:58:27 +0530

    Frontend Files Pushed to Autodeployment

---
+++ src/services/user.service.ts
@@ -0,0 +1,25 @@
+const API_URL = "/api";
+
+export const getUsers = async () => {
+  const res = await fetch(`${API_URL}/users`, {
+    cache: "no-store",
+  });
+
+  if (!res.ok) throw new Error("Failed to fetch users");
+
+  return res.json();
+};
+
+export const createUser = async (data: { name: string; email: string }) => {
+  const res = await fetch(`${API_URL}/users/addUsers`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+
+  if (!res.ok) throw new Error("Failed to create user");
+
+  return res.json();
+};
