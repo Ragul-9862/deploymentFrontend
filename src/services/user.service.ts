924f264d686e92f82c6c96be6d5b8165f46f2116
ragul-9862 <ragulragul9862@gmail.com>
2025-12-29 12:42:05 +0530

    refactor ecosystem.config.js

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,11 +1,12 @@
-const API_URL = "http://apidev.cloud/api/api";
+// const API_URL = "http://apidev.cloud/api/api";
+const API_URL = "http://localhost:3000/api";
 
 
 export const getUsers = async () => {
   const res = await fetch(`${API_URL}/users/get`, {
     cache: "no-store",
   });
-
+console.log(res);
   if (!res.ok) throw new Error("Failed to fetch users");
 
   return res.json();
