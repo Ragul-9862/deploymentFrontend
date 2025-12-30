282448a9c2b6322304e6a6f2b89f7abbb18c17f8
ragul-9862 <ragulragul9862@gmail.com>
2025-12-30 09:38:39 +0530

    url updated-new

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,6 +1,6 @@
-// const API_URL = "https://apidev.cloud/api";
-const API_URL = "http://localhost:5000";
-
+const API_URL = "https://apidev.cloud/api";
+// const API_URL = "http://localhost:5000";
+// 
 
 export const getUsers = async () => {
 
